d634734d3191e972886c91f3426db7f8adb15f3b
slimaniabdelhadi <slimani.abdelhadi08@gamil.com>
2024-11-16 02:34:27 +0100

    abbdd

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,13 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <h1>abdelhadi</h1>
+        <h1>abdelhadi slimani</h1>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>abdelhadi</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
